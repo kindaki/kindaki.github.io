3dddd8fe5f89078f6f77b085b3dce6da22343111
Zhuwench <1433329493@qq.com>
2024-08-09 21:58:50 +0800

    Site updated: 2024-08-09 21:58:50

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1723211898638";
+const VERSION = "1723211929935";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
